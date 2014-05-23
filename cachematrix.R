bfbb3691b6ffd42e9c031ac61a930f6a7b138f5b
jsihota <jagdeep_s1@hotmail.com>
2014-05-22 18:55:22 -0700

    added logic for cache

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## caching is used to inverse of a matrix rather than compute it repeatedly
 
 ## cache the inverse of matrix if it is not changed 
-
 makeCacheMatrix <- function(x = matrix()) {
+
+
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m <<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 
 }
 
 
 ## computes the inverse of the matrix
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    meesage("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
+  
 }
